36d273af1555a0a462837a685a75f85dba3702be
TayaVoin <taya@voinov.info>
2025-05-21 12:04:25 +0700

    Add files via upload

---
+++ Semester_1/Lab12_MakeCmake/task1/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/student/Gosha/Практика 12/task1/src/main.c" "CMakeFiles/main.dir/src/main.c.o" "gcc" "CMakeFiles/main.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/student/Gosha/Практика 12/task1/build/background/CMakeFiles/background.dir/DependInfo.cmake"
+  "/media/student/Gosha/Практика 12/task1/build/pipes/CMakeFiles/pipes.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
